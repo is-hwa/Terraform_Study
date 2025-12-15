8e598a7bda4f3cac159a05e42c799c05b850c5bb
is-hwa <is-hwa@naver.com>
2025-12-15 20:14:46 +0900

    studying for a data source

--- 02/webserver-cluster/main.tf
+++ 02/webserver-cluster/main.tf
@@ -9,3 +9,21 @@
 # * launch Tmeplate 생성 
 # * ASG 생성
 #################################
+
+#########################
+# 0. infra
+#########################
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
+
+data "aws_vpc" "selected" {
+  default = true
+}
+
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
