a48a3f430d26d8c8621b88aeb570111d645af907
is-hwa <is-hwa@naver.com>
2025-12-15 20:20:52 +0900

    Auto Scailing with ALB

--- 02/webserver-cluster/main.tf
+++ 02/webserver-cluster/main.tf
@@ -1,67 +1,47 @@
-#################################
-# 1. ALB
-# * SG 생성
-# * TG 생성
-# * ALB 생성
-# * ALB Listener 생성
-# 2. ASG
-# * SG 생성
-# * launch Tmeplate 생성 
-# * ASG 생성
-#################################
-
-#########################
-# 0. infra
-#########################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
-
-data "aws_vpc" "selected" {
-  default = true
-}
-
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-#########################
-# 1. ALB
-#########################
-# * SG 생성
-# * TG 생성
-# * ALB 생성
-# * ALB Listener 생성
-
-#########################
-# * SG 생성
-#########################
-# ingress: 80/tcp, egress: all
-
 resource "aws_security_group" "myalb_sg" {
   name        = "myalb_sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.default.id
+  description = "Allow HTTP inbound and all outbound traffic"
+  vpc_id      = data.aws_vpc.default.id
 
   tags = {
-    Name = "allow_tls"
+    Name = "myalb_sg"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_80" {
+resource "aws_vpc_security_group_ingress_rule" "allow_myalb_80" {
   security_group_id = aws_security_group.myalb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
+  from_port         = var.web_port
+  to_port           = var.web_port
   ip_protocol       = "tcp"
 }
 
-
-resource "aws_vpc_security_group_egress_rule" "allow_all" {
+resource "aws_vpc_security_group_egress_rule" "allow_myalb_all" {
   security_group_id = aws_security_group.myalb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
+}
+
+resource "aws_security_group" "myasg_sg" {
+  name        = "myasg_sg"
+  description = "Allow HTTP inbound and all outbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "myasg_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_myasg_80" {
+  security_group_id = aws_security_group.myasg_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = var.web_port
+  to_port           = var.web_port
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_myasg_all" {
+  security_group_id = aws_security_group.myasg_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
---
+++ 02/webserver-cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "web_port" {
+  default = 80
+}
+
+variable "aws" {
+  default = "137112412989"
+}
+
+variable "vCPU2_MEM1g" {
+  default = "t3.micro"
+}
