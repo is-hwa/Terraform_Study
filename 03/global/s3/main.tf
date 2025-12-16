50853af5faa40a4ffb1c351dc86447d174d41072
is-hwa <is-hwa@naver.com>
2025-12-16 21:54:10 +0900

    create s3 and connect

---
+++ 03/global/s3/main.tf
@@ -0,0 +1,28 @@
+##############################
+# 1. S3 bucket 생성
+# 2. DynamoDB table 생성
+##############################
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# resource "aws_s3_bucket" "mytfstate" {
+#   bucket = "lsh-2393"
+
+#   tags = {
+#     Name        = "lsh-2393"
+#   }
+# }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "my_tflocks" {
+  name           = "my_tflocks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "my_tflocks"
+  }
+}
---
+++ 03/global/s3/outputs.tf
@@ -0,0 +1,7 @@
+# output "mybucket_arn" {
+#   value = aws_s3_bucket.mytfstate.arn
+# }
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.my_tflocks.name
+}
